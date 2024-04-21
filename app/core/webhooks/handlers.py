b7e0db69c31a1f224335520e0dc8d253c1dc1218
Mikhail Khromov <khromov05@gmail.com>
2024-04-21 12:21:55 +0300

    add redis to endpoints

--- app/core/webhooks/handlers.py
+++ app/core/webhooks/handlers.py
@@ -1,31 +1,42 @@
+import json
 import logging
+from typing import Any
 
+import user_agents
+from core.redis import get_by_key, set_by_key
+from core.webhooks.models import UserPredictionsResponse, UserUpdateData
 from fastapi import APIRouter, status
-from pydantic import BaseModel
 
 router = APIRouter(prefix="/api/v1", tags=["v1"])
 logger = logging.getLogger(__name__)
 
 
-class UserUpdateData(BaseModel):
-    timstamp: int
-    geo_id: int
-    domain_id: int
-    path_id: int
-    user_agent: str
-
-
-class UserPredictionsResponse(BaseModel):
-    gender: int
-    age: int
-
-
 @router.get("/user/{user_id}/predictions", response_model=UserPredictionsResponse)
 async def get_predictions(user_id: int):
+    user_data = await get_by_key(f"user:{user_id}")
+    logger.info(f"User data for id={user_id}: {user_data}")
     return {"gender": 1, "age": 42}
 
 
 @router.put("/user/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def update_user(user_id: int, body: UserUpdateData):
-    return 
-    
+    parsed = user_agents.parse(body.user_agent)
+    browser, os, device, is_mobile = parsed.browser.family, parsed.os.family, parsed.device.family, parsed.is_mobile
+    data: dict[str, Any] = body.model_dump()
+    # add browser, os, device, is_mobile
+    data["browser"] = browser
+    data["os"] = os
+    data["device"] = device
+    data["is_mobile"] = is_mobile
+    # save to redis
+    data_list = await get_by_key(f"user:{user_id}")
+    if data_list:
+        data_list = json.loads(data_list)
+        data_list.append(data)
+    else:
+        data_list = [data]
+    await set_by_key(
+        f"user:{user_id}",
+        json.dumps(data_list),
+    )
+
--- app/core/webhooks/models.py
+++ app/core/webhooks/models.py
@@ -1,4 +1,15 @@
 from pydantic import BaseModel
 
 
+class UserUpdateData(BaseModel):
+    timstamp: int
+    geo_id: int
+    domain_id: int
+    path_id: int
+    user_agent: str
 
+
+class UserPredictionsResponse(BaseModel):
+    gender: int
+    age: int
+    
